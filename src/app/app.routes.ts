7371b27c5a45eaa27f444f1f8eb1229ecf6dbd2d
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-26 20:55:00 -0300

    feat: home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/services/pages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
