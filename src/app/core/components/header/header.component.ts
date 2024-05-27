5e6e3956063c8a30c192bc977d3a41cb2188d1e2
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-26 21:04:49 -0300

    feat: logo at header component

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'header-component',
   template: `
-  <div id="header-component"></div>
+  <div id="header-component">
+    <h3>PORTAL GRP</h3>
+  </div>
   `,
   styleUrl: './header.component.scss'
 })
