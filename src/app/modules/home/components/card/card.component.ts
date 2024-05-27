36539f1f11e617ac0eb53ad18ed41dc5d5864320
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-26 22:15:23 -0300

    feat: if dont have a decimal value, put 99 at the end

--- src/app/modules/home/components/card/card.component.ts
+++ src/app/modules/home/components/card/card.component.ts
@@ -6,32 +6,42 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [CommonModule],
   template: `
-  <div class="card-component">
-    <div class="card-image" *ngIf="imageName">
-      <img src="assets/img/{{imageName}}" />
+    <div class="card-component">
+      <div class="card-image" *ngIf="imageName">
+        <img src="assets/img/{{ imageName }}" />
+      </div>
+      <div class="card-content">
+        <span class="card-title">{{ title }}</span>
+        <p>{{ description }}</p>
+      </div>
+      <h4>{{ isValidPrice(price) }}</h4>
+      <div class="card-action">
+        <a href="{{ link }}">Fazer proposta.</a>
+      </div>
     </div>
-    <div class="card-content">
-      <span class="card-title">{{title}}</span>
-      <p>{{description}}</p>
-    </div>
-    <h4>{{isValidPrice(price)}}</h4>
-    <div class="card-action">
-      <a href="{{link}}">Fazer proposta.</a>
-    </div>
-  </div>
   `,
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
 })
 export class CardComponent {
-  @Input() public imageName: string = "";
-  @Input() public title: string = "";
-  @Input() public description: string = "";
-  @Input() public price: string = "";
-  @Input() public link: string = "";
+  @Input() public imageName: string = '';
+  @Input() public title: string = '';
+  @Input() public description: string = '';
+  @Input() public price: string = '';
+  @Input() public link: string = '';
 
   isValidPrice(price: string): string {
-    if (isNaN(parseFloat(price))) return ""
+    const trimmedPrice = price.trim();
 
-    return "R$ " + parseFloat(price).toFixed(2);
+    if (isNaN(parseFloat(trimmedPrice))) return '';
+
+    if (!trimmedPrice.includes('.')) {
+      return (
+        'R$ ' + parseFloat(trimmedPrice).toFixed(0).replace('.', ',') + ',99'
+      );
+    }
+    const formattedPrice = parseFloat(trimmedPrice)
+      .toFixed(2)
+      .replace('.', ',');
+    return 'R$ ' + formattedPrice;
   }
 }
