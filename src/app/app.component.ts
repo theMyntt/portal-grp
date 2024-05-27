3907defcbbbf943128bac2527d96f85d60fb51f8
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-26 21:38:02 -0300

    fix: importing home module at default component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeModule } from './modules/home/home.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeModule],
   template: `
   <div>
     <router-outlet></router-outlet>
